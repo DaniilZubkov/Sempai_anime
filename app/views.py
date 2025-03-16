ebf54a36849b4576708f0815e9556ce92688d08d
DaniilZubkov <zubkovde@mail.ru>
2025-03-16 21:14:01 +0400

    end_of_kino

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from animes.models import AnimeTitle, Anime
 
 def index(request):
+    anime_titles = Anime.objects.all()[:5]
     context = {
-        'titile': 'Sempai Anime'
+        'titile': 'Sempai Anime',
+        'anime_titles': anime_titles,
     }
     return render(request, 'app/index.html', context)
